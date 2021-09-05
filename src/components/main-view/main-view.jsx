1dfe2de99563d8611a1e88181f15c33bb92c52b2
MitoMonkey <85686133+MitoMonkey@users.noreply.github.com>
2021-09-05 12:07:28 +0200

    MainView state constructor and added to render()

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -2,12 +2,33 @@ import React from 'react';
 
 export class MainView extends React.Component {
 
+    constructor() {
+        super();
+        this.state = {
+            movies: [
+                { _id: 1, Title: 'Inception', Description: 'desc1...', ImagePath: '...' },
+                { _id: 2, Title: 'The Shawshank Redemption', Description: 'desc2...', ImagePath: '...' },
+                { _id: 3, Title: 'Gladiator', Description: 'desc3...', ImagePath: '...' }
+            ]
+        }
+    }
+
     render() {
+        /*
         return (
             <div className="main-view">
                 <div>Inception</div>
                 <div>The Shawshank Redemption</div>
                 <div>Gladiator</div>
+            </div>
+        );
+        */
+        const { movies } = this.state; // Short for ´const movies = this.state.movies;´ (ES6 object destruction)
+        if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+
+        return (
+            <div className="main-view">
+                {movies.map(movie => <div key={movie._id}>{movie.Title}</div>)}
             </div>
         );
     }
--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import MainView from './components/main-view/main-view';
 
-// Import statement to indicate that you need to bundle `./index.scss`
 import './index.scss';
 
 // Main component (will eventually use all the others)
@@ -19,8 +18,8 @@ class MoveXApplication extends React.Component {
     }
 }
 
-// Finds the root of the app
+// Find the root of the app
 const container = document.getElementsByClassName('app-container')[0];
 
-// Tells React to render the app in the root DOM element
+// Tell React to render the app in the root DOM element
 ReactDOM.render(React.createElement(MoveXApplication), container);
