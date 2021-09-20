37dab43428b3ae0f8f6b24dc5779c353cc430c2f
MitoMonkey <85686133+MitoMonkey@users.noreply.github.com>
2021-09-20 18:30:44 +0200

    setup redux-devtools-extension

--- src/index.jsx
+++ src/index.jsx
@@ -4,11 +4,12 @@ import MainView from './components/main-view/main-view';
 import Container from 'react-bootstrap/Container';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import moviesApp from './reducers/reducers';
+import movesApp from './reducers/reducers';
+import { devToolsEnhancer } from 'redux-devtools-extension';
 
 import './index.scss';
 
-const store = createStore(moviesApp);
+const store = createStore(movesApp, devToolsEnhancer());
 
 class MoveXApplication extends React.Component {
     render() {
